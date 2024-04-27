006ee6b2d4e74e899a3be04eb79fd31c7b47be12
Gustavo Guimarães da Silva <gusguims@outlook.com>
2024-04-27 10:48:18 -0300

    Começando a colocar peças no tabuleiro

--- Xadrez/Tabuleiro/Peca.cs
+++ Xadrez/Tabuleiro/Peca.cs
@@ -13,9 +13,9 @@ namespace _Tabuleiro
         public int QtdMovimentos { get; protected set; }
         public Tabuleiro Tabuleiro { get; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        public Peca(Cor cor, Tabuleiro tabuleiro)
         {
-            Posicao = posicao;
+            Posicao = null;
             Cor = cor;
             QtdMovimentos = 0;
             Tabuleiro = tabuleiro;

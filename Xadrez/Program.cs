802339edbbc15eb1ee307abd3bbc2b02a96cff05
Gustavo Guimarães da Silva <gusguims@outlook.com>
2024-05-10 19:14:14 -0300

    melhorando a implementação do método ImprimirTabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -9,9 +9,24 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            try
+            {
+                _Tabuleiro.Tabuleiro tab = new _Tabuleiro.Tabuleiro(8, 8);
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 3));
+                
+                tab.ColocarPeca(new Rei(tab, Cor.Branca), new Posicao(5, 3));
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(2, 4));
+                Tela.ImprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             
-            Console.WriteLine(pos.ToPosicao());
+
+
         }
     }
 }
